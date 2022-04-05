514ab02961018afc52520530920a657ef2e6ce4d
Nicholas Matison <nmatison@gmail.com>
2022-04-04 21:13:25 -0400

    added coming soon for the unmade charts

--- javascripts/util.js
+++ javascripts/util.js
@@ -11,15 +11,20 @@ const handleChartSelectionClick = (listElement) => {
       break;
 
     default:
-      return renderStart();
+      return svg.id === "default" ? null : renderStart(svg);
       break;
   }
 };
 
-const renderStart = () => {};
+const renderStart = (svg) => {
+  svg.id = "default";
+
+  d3.select("svg").selectAll("*").remove();
+  svg.style.background = "rgb(169,169,169)";
+  svg.innerHTML += `<text x="345" y="480" class="svg-default-text">Coming Soon!!! (TM)</text>`;
+};
 
 const handleZoomBubbleChartSelection = (svg) => {
   svg.id = ZOOM_BUBBLE_CHART_CLASS_NAME;
   renderZoomBubbleChart();
-
 };
